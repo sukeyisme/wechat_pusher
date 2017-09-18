2f164865e11bcb84c70b5267a3a7b468bda71aff
hodge <hodge.shen@gmail.com>
2017-09-18 19:03:51 +0800

    modle

--- models/message.go
+++ models/message.go
@@ -1,22 +1,21 @@
 package models
 
 type Message struct {
-	ToUser string `json:"touser"`
+	ToUser     string `json:"touser"`
 	TemplateId string `json:"template_id"`
-	Url string `json:"url"`
-	Data Data `json:"data"`
+	Url        string `json:"url"`
+	Data       Data   `json:"data"`
 }
 
 type Data struct {
-	First Raw `json:"first"`
-	Subject Raw `json:"subject"`
-	Sender Raw `json:"sender"`
-	Remark Raw `json:"remark"`
+	First    Raw `json:"first"`
+	Keyword1 Raw `json:"keyword1"`
+	Keyword2 Raw `json:"keyword2"`
+	Keyword3 Raw `json:"keyword3"`
+	Remark   Raw `json:"remark"`
 }
 
 type Raw struct {
 	Value string `json:"value"`
 	Color string `json:"color"`
 }
-
-
